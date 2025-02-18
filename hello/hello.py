5bea2dd1c8e9b0ec46d679b24e101ed13a96005c
Mohammad Faqusa <103820177+mohammad0faqusa@users.noreply.github.com>
2025-02-18 19:02:44 +0200

    Update hello.py

--- hello/hello.py
+++ hello/hello.py
@@ -1,1 +1,10 @@
 
+"""
+my_library.py - A simple MicroPython library for ESP32.
+"""
+
+def greet(name):
+    return f"Hello, {name}! Welcome to MicroPython on ESP32."
+
+def add(a, b):
+    return a + b
