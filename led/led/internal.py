36068184106b12d35dbc04d0276df9dbc235f317
Mohammad Faqusa <201014@ppu.edu.ps>
2025-04-18 08:20:30 +0300

    edit the class of InternalLED

--- led/led/internal.py
+++ led/led/internal.py
@@ -1,7 +1,7 @@
 from .driver import LED
 
 class InternalLED(LED):
-    def __init__(self, simulate=True):
+    def __init__(self, simulate=False):
         # On ESP32, internal LED is usually on GPIO2 and active high
         super().__init__(pin=2, active_high=True, simulate=simulate)
         print("[INIT] InternalLED initialized on pin 2")
