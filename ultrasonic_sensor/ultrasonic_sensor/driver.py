6ff50f91c7d9276ac29c273e8d8e0591e29c7d27
Mohammad Faqusa <201014@ppu.edu.ps>
2025-06-11 10:19:22 +0300

    ultra sonic sensor: add method wrapper

--- ultrasonic_sensor/ultrasonic_sensor/driver.py
+++ ultrasonic_sensor/ultrasonic_sensor/driver.py
@@ -1,11 +1,29 @@
 import machine
 import time
 
+class MethodWrapper:
+    def __init__(self, func):
+        self.func = func
+        
+     # NEW: make the wrapper itself callable
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __getitem__(self, args):
+        if isinstance(args, (list, tuple)):
+            return self.func(*args)  # Unpack!
+        else:
+            return self.func(args)
+        
 class UltrasonicSensor:
     def __init__(self, trig_pin, echo_pin):
         self.trig = machine.Pin(trig_pin, machine.Pin.OUT)
         self.echo = machine.Pin(echo_pin, machine.Pin.IN)
         self.trig.off()
+        
+    def __getitem__(self, key):
+        method = getattr(self, key)
+        return MethodWrapper(method)
 
     def get_distance(self):
         self.trig.off()
